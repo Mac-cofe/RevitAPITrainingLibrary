50d0ea1897ab6778c6443dd5e6b6d2c6b9ef0390
M.M.Karmanova@urfu.me <M.M.Karmanova@urfu.me>
2022-02-09 13:13:31 +0500

    Библиотека

--- RevitAPITrainingLibrary/SelectionUtils.cs
+++ RevitAPITrainingLibrary/SelectionUtils.cs
@@ -11,38 +11,85 @@ namespace RevitAPITrainingLibrary
 {
     public class SelectionUtils
     {
-        public static Element PickObject(ExternalCommandData commandData, string message = "Выберите элемент")
-        {
-            UIApplication uiapp = commandData.Application;
-            UIDocument uidoc = uiapp.ActiveUIDocument;
-            Document doc = uidoc.Document;
-
-            var selectedObject = uidoc.Selection.PickObject(ObjectType.Element, message);
-            var oElement = doc.GetElement(selectedObject);
-            return oElement;
-        }
-        public static List<Element> PickObjects(ExternalCommandData commandData, string message = "Выберите элементы")
-        {
-            UIApplication uiapp = commandData.Application;
-            UIDocument uidoc = uiapp.ActiveUIDocument;
-            Document doc = uidoc.Document;
-
-            List<Element> elementList = null;
-            try
+    
+            public static Element PickObject(ExternalCommandData commandData, string message = "Выберите элемент")
             {
-               
-                var selectedObjects = uidoc.Selection.PickObjects(ObjectType.Element, message);
-                elementList = selectedObjects.Select(selectedObject => doc.GetElement(selectedObject)).ToList();
-                
+                UIApplication uiapp = commandData.Application;
+                UIDocument uidoc = uiapp.ActiveUIDocument;
+                Document doc = uidoc.Document;
+
+                var selectedObject = uidoc.Selection.PickObject(ObjectType.Element, message);
+                var oElement = doc.GetElement(selectedObject);
+                return oElement;
             }
-            catch (Autodesk.Revit.Exceptions.OperationCanceledException) { }
-            
 
-            return elementList;
+            public static T GetObject<T>(ExternalCommandData commandData, string promptMessage)
+            {
+                UIApplication uiapp = commandData.Application;
+                UIDocument uidoc = uiapp.ActiveUIDocument;
+                Document doc = uidoc.Document;
+                Reference selectedObj = null;
+                T elem;
+                try
+                {
+                    selectedObj = uidoc.Selection.PickObject(ObjectType.Element, promptMessage);
+                }
+                catch (Exception)
+                {
+                    return default(T);
+                }
+                elem = (T)(object)doc.GetElement(selectedObj.ElementId);
+                return elem;
+            }
 
+            public static List<Element> PickObjects(ExternalCommandData commandData, string message = "Выберите элементы")
+            {
+                UIApplication uiapp = commandData.Application;
+                UIDocument uidoc = uiapp.ActiveUIDocument;
+                Document doc = uidoc.Document;
+
+                var selectedObjects = uidoc.Selection.PickObjects(ObjectType.Element, message);
+                List<Element> elementList = selectedObjects.Select(selectedObject => doc.GetElement(selectedObject)).ToList();
+                return elementList;
+            }
+
+            public static List<XYZ> GetPoints(ExternalCommandData commandData,
+                                string promptMessage, ObjectSnapTypes objectSnapTypes)
+            {
+                UIApplication uiapp = commandData.Application;
+                UIDocument uidoc = uiapp.ActiveUIDocument;
+
+                List<XYZ> points = new List<XYZ>();
+
+                while (true)
+                {
+                    XYZ pickedPoint = null;
+                    try
+                    {
+                        pickedPoint = uidoc.Selection.PickPoint(objectSnapTypes, promptMessage);
+                    }
+                    catch (Autodesk.Revit.Exceptions.OperationCanceledException ex)
+                    {
+                        break;
+                    }
+                    points.Add(pickedPoint);
+                }
+
+                return points;
+            }
+
+            public static XYZ GetPoint(ExternalCommandData commandData,
+                                string promptMessage, ObjectSnapTypes objectSnapTypes)
+            {
+                UIApplication uiapp = commandData.Application;
+                UIDocument uidoc = uiapp.ActiveUIDocument;
+                Document doc = uidoc.Document;
+
+                var pickedPoint = uidoc.Selection.PickPoint(ObjectSnapTypes.Endpoints, "Выберите точку");
+
+                return pickedPoint;
+            }
         }
-
-
     }
-}
+
 
